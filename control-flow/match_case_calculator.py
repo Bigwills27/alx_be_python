b634139f3d16ffd289baa20dcad35c798b23b54d
Bigwills <ajbodesecond@gmail.com>
2025-05-30 20:00:01 +0100

    first control flow

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,12 +13,10 @@ match operation:
         result = num1 * num2
         print(f"The result is {result}")
     case '/':
-        # Handle division by zero case.
         if num2 != 0:
             result = num1 / num2
             print(f"The result is {result}")
         else:
             print("Cannot divide by zero.")
-    # Handle any unexpected operation input.
     case _:
         print("Invalid operation. Please choose from +, -, *, or /.")
